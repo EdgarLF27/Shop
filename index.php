3ee3dfc57041258ec8f95bf2a11ceeef10a16bc5
Edgar Flores <e.flores30nov@gmail.com>
2024-10-16 22:46:35 -0600

    start

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Shop </title>
+    <h2>Bienvenido a tu tienda de preferencia </h2>
+</head>
+
+<body>
+    <?php
+
+    ?>
+</body>
+
+</html>
